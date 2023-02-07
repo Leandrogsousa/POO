ac28974ec887b490a77ff97456c2806cd86217bc
Leandro Sousa <leandrogomyys@gmail.com>
2023-02-06 21:51:13 -0300

    Projeto Montadora
    Continuação do projeto.

--- Montadora/ConsoleAppPrincipal/Program.cs
+++ Montadora/ConsoleAppPrincipal/Program.cs
@@ -6,30 +6,35 @@ namespace ConsoleAppPrincipal
     {
         static void Main(string[] args)
         {
-            Pneu pneu1 = new Pneu(16,150, "carro de passeio", false);
+            // Pneu pneu1 = new Pneu(16,150, "carro de passeio", false);
+            //Pneu pneu2 = new Pneu(16,70,"Pneu de estepe", false);
+            //Carro corcel = new Carro("Toro","Fiat",2022,180,"WEF-2134");
+
+            Carro fusca = new Carro("Volkswagen", "Itamar Franco", 1994, 120, "SWK-1234");
             
-            Pneu pneu2 = new Pneu(16,70,"Pneu de estepe", false);
-            
-            Carro corcel = new Carro("Toro","Fiat",2022,180,"WEF-2134");
+            fusca.Abastecer(80);
+            fusca.Ligar();
+            fusca.Acelerar(15);
+            fusca.Acelerar(5);
+            fusca.Acelerar(22);
+            fusca.Frear(8);
+            fusca.Acelerar(3);
+            fusca.Frear(4);
+            fusca.Desligar();
+            fusca.PneuDianteiroEsquerdo = fusca.PneuEstepe;
+            fusca.Ligar();
+            fusca.Acelerar(15);
+            fusca.Acelerar(5);
+            fusca.Acelerar(22);
+            fusca.Frear(8);
+            fusca.Acelerar(3);
+            fusca.Frear(4);
+            fusca.Acelerar(60);
+            fusca.Acelerar(8);
+            fusca.Acelerar(29);
 
+           fusca.Exibir();
 
-
-
-
-
-
-            corcel.Abastecer();
-            pneu1.Girar(6);
-            pneu2.Girar(6);
-            pneu2.Girar(15);
-            pneu2.Girar(10);
-            pneu2.Freiar(5);
-            pneu2.Girar(20);
-            pneu2.Girar(20);
-            pneu2.Girar(5);
-            pneu1.Exibir();
-            Console.WriteLine();
-            pneu2.Exibir();
         }
        
     }
--- Montadora/Models/Carro.cs
+++ Montadora/Models/Carro.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Models
 {
-     public class Carro
+    public class Carro
     {   //Atributos
         /// <summary>
         /// Aqui onde se coloca o modelo do carro.
@@ -31,7 +31,7 @@ namespace Models
         public Pneu PneuEstepe { get; set; }
 
         //Construtor
-        public Carro(string _modelo, string _marca, int _ano, int _velocidademaxima, string _placa) 
+        public Carro(string _modelo, string _marca, int _ano, int _velocidademaxima, string _placa)
         {
             Modelo = _modelo;
             Marca = _marca;
@@ -46,38 +46,127 @@ namespace Models
             PneuDianteiroEsquerdo = new Pneu(16, 150, "Carro de passeio");
             PneuTraseiroDireito = new Pneu(16, 150, "Carro de passeio");
             PneuTraseiroEsquerdo = new Pneu(16, 150, "Carro de passeio");
-            PneuEstepe = new Pneu(16,70,"Estepe",true);
+            PneuEstepe = new Pneu(16, 70, "Estepe", true);
 
         }
         public void Ligar()
         {
-                if (Combustivel < 0)
+            if (Combustivel > 0)
+            {
+                Combustivel -= 3;
+                Ligado = true;
+
+                if (Combustivel <=0)
                 {
-                    Ligado = true;
+                    Combustivel = 0;
+                    Desligar();
                 }
-            
+            }
+           
+
+
         }
         public void Desligar()
         {
             Ligado = false;
-            VelocidadeAtual = 0;
+            Parar();
+        }
+
+        public void Acelerar(int _impulso)
+        {
+            if (Ligado == true && _impulso > 0)
+            {
+                Odometro += 18;
+                Combustivel = Combustivel - 8;
+
+                if (Combustivel <= 0)
+                {
+                    Combustivel = 0;
+                    Desligar();
+                    return;
+                    
+                }
+
+
+                VelocidadeAtual = VelocidadeAtual + _impulso;
+                PneuDianteiroDireito.Girar(_impulso);
+                PneuDianteiroEsquerdo.Girar(_impulso);
+                PneuTraseiroDireito.Girar(_impulso);
+                PneuTraseiroEsquerdo.Girar(_impulso);
+                if (PneuDianteiroDireito.Estourado || PneuDianteiroDireito.Estourado ||PneuTraseiroDireito.Estourado || PneuTraseiroEsquerdo.Estourado)
+                {
+                    Parar();
+                }
+            }
+
+        }
+        private void Parar()
+        {
             PneuDianteiroDireito.VelocidadeAtual = 0;
             PneuDianteiroEsquerdo.VelocidadeAtual = 0;
             PneuTraseiroDireito.VelocidadeAtual = 0;
             PneuTraseiroEsquerdo.VelocidadeAtual = 0;
         }
-        public void Abastecer()
+        public void Frear(int _reduzir)
         {
+            VelocidadeAtual = VelocidadeAtual - _reduzir;
+
+            if (VelocidadeAtual <= 0)
+                VelocidadeAtual = 0;
+            PneuDianteiroDireito.Freiar(_reduzir);
+            PneuDianteiroEsquerdo.Freiar(_reduzir);
+            PneuTraseiroDireito.Freiar(_reduzir);
+            PneuTraseiroEsquerdo.Freiar(_reduzir);
+
 
         }
-
-        public void Acelerar()
+        /// <summary>
+        /// Este método vao abastecer o carro.
+        /// </summary>
+        /// <param name="_quantidadeCombustivel">Informe o percentual de combustível que deseja abastecer, caso informe 0,
+        /// o método irá completar o tanque. O valor não pode ser superior a 100.</param>
+        public void Abastecer(int _quantidadeCombustivel = 0)
         {
+            if (_quantidadeCombustivel == 0)
+                _quantidadeCombustivel = 100 - Combustivel;
+
+
+
+            if (Combustivel + _quantidadeCombustivel > 100)
+            {
+                Console.WriteLine("A quantidade de combustível ultrapassa o limite de capacidade");
+                return;
+            }
+            if (Combustivel < 100)
+                Combustivel = Combustivel + _quantidadeCombustivel;
 
         }
-        public void Frear()
+        public void Exibir()
         {
+            Console.Clear();
+            Console.WriteLine("Marca:" + Modelo);
+            Console.WriteLine("Tipo: " + Tipo);
+            Console.WriteLine("Ano: " + Ano);
+            Console.WriteLine("COmbustível: " + Combustivel);
+            Console.WriteLine("Capacidade do Tanque: " + CapacidadeTanque);
+            Console.WriteLine("Velocidade Máxima: " + VelocidadeMaxima);
+            Console.WriteLine("Velocidade Atual: " + VelocidadeAtual);
+            Console.WriteLine("Marca: " + Marca);
+            Console.WriteLine("Cor: " + Cor);
+            Console.WriteLine("Placa: " + Placa);
+            Console.WriteLine("Odometro: " + Odometro);
+            Console.WriteLine("Ligado: " + Ligado);
 
+            Console.WriteLine("\nPneuDianteiroEsquerdo");
+            PneuDianteiroEsquerdo.Exibir();
+            Console.WriteLine("\nPneuDianteiroDireito");
+            PneuDianteiroDireito.Exibir();
+            Console.WriteLine("\nPneuTraseiroEsquerdo");
+            PneuTraseiroEsquerdo.Exibir();
+            Console.WriteLine("\nPneuTraseiroDireito");
+            PneuTraseiroDireito.Exibir();
+            Console.WriteLine("\nPneuEstepe");
+            PneuEstepe.Exibir();    
         }
     }
 
